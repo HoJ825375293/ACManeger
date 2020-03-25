4ede1f12ef36166ebf9e63e63bcba421404ec396
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-03-25 17:23:17 +0800

    -

---
+++ BackEnd/src/main/java/com/acmaneger/maneger/ManegerApplication.java
@@ -0,0 +1,13 @@
+package com.acmaneger.maneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManegerApplication.class, args);
+    }
+
+}
