a4330d1140af8bf81ea7c85c3c3f288965c39e96
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-03-25 22:09:36 +0800

    -

--- BackEnd/src/main/java/com/hotel/acmaneger/ACManager.java
+++ BackEnd/src/main/java/com/hotel/acmaneger/ACManager.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ACmng {
+public class ACManager {
 
     public static void main(String[] args) {
-        SpringApplication.run(ACmng.class, args);
+        SpringApplication.run(ACManager.class, args);
     }
 
 }
