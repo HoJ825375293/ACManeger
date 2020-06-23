7ff7f875d703ad7ab8e7d83980db7320b59f5d65
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-06-23 22:21:23 +0800

    -

--- FrontEnd/src/components/AdminPage.js
+++ FrontEnd/src/components/AdminPage.js
@@ -16,16 +16,44 @@ import {
   Progress,
   Slider,
   InputNumber,
-  Menu
+  Menu,
+  Table
 } from "antd";
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Redirect, Link } from "react-router-dom";
 import AccountBar from './AccountBar'
-import UserAvartar from '../PIC/u50.svg'
+import AdminAvartar from '../PIC/u177.svg'
+
+const data = [
+  {
+    key:"1",
+    room:301,
+    state:"正在运行"
+  },{
+    key:"2",
+    room:302,
+    state:"等待中"
+  },{
+    key:"3",
+    room:303,
+    state:"关闭"
+  }
+]
 
 class AdminPage extends React.Component {
     constructor(){
         super()
+
+        this.columns = [
+          {
+            title: '房间',
+            dataIndex: 'room',
+            width: '30%',
+          },
+          {
+            title: '状态',
+            dataIndex: 'state',
+          },]
 
         this.state={
             startTime:new Date(),
@@ -42,6 +70,7 @@ class AdminPage extends React.Component {
 
     render(){
         const {startTime, time, name} = this.state
+        const columns = this.columns
 
         return(
             <div>
@@ -99,13 +128,13 @@ class AdminPage extends React.Component {
                     >
                     <div>
                     <Row>
-                    <Avatar style={{width: 120, height:100 }} src={UserAvartar} />
+                    <Avatar style={{width: 120, height:100 }} src={AdminAvartar} />
                     <span style={{marginLeft:40, fontSize:25, fontWeight:"bold"}}>管理员-{name}</span> 
                     </Row>
                     </div>
                     <Divider/>
                     <div style={{marginLeft:50}}>
-                    <Row><span style={{marginLeft:40, fontSize:20}}>入住时间: {startTime.toLocaleTimeString()}</span></Row>
+                    <Row><span style={{marginLeft:40, fontSize:20}}>登录时间: {startTime.toLocaleTimeString()}</span></Row>
                     <Row style={{marginTop:5}}><span style={{marginLeft:40,fontSize:20}}>现在时间: {time.toLocaleTimeString()}</span></Row>
                     </div>
                     </Card>
@@ -113,93 +142,23 @@ class AdminPage extends React.Component {
                     <Card
                         style={{ width: 480, height:200, marginTop:15 }}
                     >
-                    <div>
-                    <Row>
-                        <Icon type="thunderbolt" style={{color:"#FFA500", marginLeft:50, fontSize:35}}/>
-                        <Icon type="transaction" style={{color:"#228B22", marginLeft:180,fontSize:35}}/>
-                    </Row>
-                    </div>
-                    <Divider/>
-                    <div>
-                    <Col span={12}><span style={{marginLeft:40, fontSize:20}}>使用电量:</span>
-                        <h2 style={{marginLeft:50}}>{elec}</h2>
-                    </Col>
-                    <Col span={12}><span style={{marginLeft:40,fontSize:20}}>需交费用:</span>
-                        <h2 style={{marginLeft:50}}>{money}</h2>
-                    </Col>
-                    </div>
+                    <Icon type="tool" style={{marginLeft:150, fontSize:80}}/>
+                    <h3 style={{marginLeft:135, fontSize:35}}>设计中</h3>
                     </Card>
                     
                     <Card
-                        style={{ width: 480, height:280, marginTop:15 }}
+                        style={{ width: 480, height:200, marginTop:15 }}
                     >
-                    <div>
-                    <Row>
-                    <h1 style={{textAlign:"center"}}>耗电与计费规则</h1>
-                    </Row>
-                    </div>
-                    <Divider/>
-                    <div>
-                    <Col span={12}><span style={{marginLeft:40, fontSize:18}}>耗电标准:</span>
-                    <h3 style={{marginLeft:40, marginTop:10}}>低风:1度/3分钟</h3>
-                    <h3 style={{marginLeft:40}}>中风:1度/2分钟</h3>
-                    <h3 style={{marginLeft:40}}>高风:1度/1分钟</h3>
-                    </Col>
-                    <Col span={12}><span style={{marginLeft:40,fontSize:18}}>计费标准:</span>
-                    <h3 style={{marginLeft:40, marginTop:10}}>1元/1度</h3>
-                    </Col>
-                    </div>
+                    <Icon type="tool" style={{marginLeft:150, fontSize:80}}/>
+                    <h3 style={{marginLeft:135, fontSize:35}}>设计中</h3>
                     </Card>
                 </Col>
                 <Col span={10}>
-                <Card
-                    style={{ width: 450 }}
-                    cover={
-                    <img
-                        alt="example"
-                        src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592926582880&di=f94879ca7b5525f6b9973240e0fbeea7&imgtype=0&src=http%3A%2F%2Fimg006.hc360.cn%2Fm3%2FM05%2FFC%2FB0%2FwKhQ5lSs7eSEU9pJAAAAAG3Iu58155.jpg"
-                    />
-                    }
-                    actions={[
-                        <Icon type="question" />,
-                        <Icon type="line-chart" />,
-                        <Icon type="ellipsis" key="ellipsis" />,
-                      ]}
-                    >
-                    <Card>
-                    <div style={{textAlign:"center"}}><Progress strokeLinecap="square" type="dashboard" percent={windForce} format={() => `${windText}`} /></div>
-                        <div style={{textAlign:"center"}}>
-                            {workingMode}
-                            <Divider type="vertical" style={{height:30, marginLeft:20, marginRight:10}}/>    
-                            {/*{temprature}℃*/}
-                            <InputNumber
-                                    min={0}
-                                    max={36}
-                                    formatter={value => `${value}℃`}
-                                    value={temprature}
-                                    onChange={this.onTemChange}
-                                    
-                            />
-                        </div>
-                        <Row>
-                            <Slider marks={marks} min={0} max={36} onChange={this.onTemChange} value={typeof temprature === 'number'? temprature:0}/>
-                        </Row>
-                    </Card>
-                    <div style={{marginTop:10,textAlign:"center"}}>
-                        <Col span={7}>
-                        <Button style={{marginLeft:50, marginTop:60}}onClick={this.ChangeWorkingMode}>模式</Button>
-                        </Col>
-                        <Col span={8}>
-                        <Card style={{ width: 100, marginLeft:35,textAlign:"center" }}>
-                            <Button onClick={this.onTemUpOne}>+</Button>
-                            <Divider />
-                            <Button onClick={this.onTemLowOne}>-</Button>
-                        </Card>
-                        </Col>
-                        <Col span={8}>
-                        <Button style={{marginLeft:10, marginTop:60}} onClick={this.ChangeWindForce}>风速</Button>
-                        </Col>
-                    </div>
+                <Card>
+                    <Table columns={columns} dataSource={data} 
+                    bordered
+                    title={() => '房间状态表'}
+                    footer={() => '更多房间待添加'}/>
                 </Card>
                 </Col>
                 </Col>
